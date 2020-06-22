2d9a3790b48e7752d8e15c193f239f8a38a7bfdc
SethBlackwater <49916389+SethBlackwater@users.noreply.github.com>
2020-06-22 16:29:42 -0500

    Added support for Kahoot sets with images.

--- convert-to-xlsx/convert.php
+++ convert-to-xlsx/convert.php
@@ -7,60 +7,45 @@ if(strpos($the_link,"=") !== false){
 $the_ID = preg_replace("/[^a-zA-Z0-9-]+/", "", $the_link);
 $the_file = file_get_contents("https://create.kahoot.it/rest/kahoots/$the_ID/card/?includeKahoot=true");
 $the_final = json_decode($the_file, true);
-for($x=0;$x<5000;$x++){
-    $the_question = $the_final['kahoot']['questions'][$x]['question'];
-    if($the_question == ''){
-        break;
+foreach($the_final['kahoot']['questions'] as $question) {
+    $the_questions[] = $question['question'];
+    $the_answer_edit='';
+    $the_answer_1[] = $question['choices'][0]['answer'];
+    $the_answer_2[] = $question['choices'][1]['answer'];
+    $the_answer_3[] = $question['choices'][2]['answer'];
+    $the_answer_4[] = $question['choices'][3]['answer'];
+    $the_correct_1[] = $question['choices'][0]['correct'];
+    $the_correct_2[] = $question['choices'][1]['correct'];
+    $the_correct_3[] = $question['choices'][2]['correct'];
+    $the_correct_4[] = $question['choices'][3]['correct'];
+    if(is_null($the_answer_3)){
+        $the_answer_3 = '';
     }
-    else{
-        $the_questions[] = $the_final['kahoot']['questions'][$x]['question'];
-        if(($the_final['kahoot']['questions'][$x]['layout']) != 'CLASSIC'){
-            $the_answer_edit='';
-            $the_answer_1[] = $the_final['kahoot']['questions'][$x]['choices'][0]['answer'];
-            $the_answer_2[] = $the_final['kahoot']['questions'][$x]['choices'][1]['answer'];
-            $the_correct_1[] = $the_final['kahoot']['questions'][$x]['choices'][0]['correct'];
-            $the_correct_2[] = $the_final['kahoot']['questions'][$x]['choices'][1]['correct'];
-            $the_answer_3[] = '';
-            $the_answer_4[] = '';
-            if($the_correct_1[$x]){
-                $the_answer_edit = '1,';
-            }
-            if($the_correct_2[$x]){
-                $the_answer_edit .= '2,';
-            }
-            $the_correct_3[] = '';
-            $the_correct_4[] = '';
-            $the_correct_answer[] = rtrim($the_answer_edit, ',');
-            $the_times[] = ($the_final['kahoot']['questions'][$x]['time']) / 1000;
-        }
-        else{
-            $the_answer_edit='';
-            $the_answer_1[$x] = $the_final['kahoot']['questions'][$x]['choices'][0]['answer'];
-            $the_answer_2[$x] = $the_final['kahoot']['questions'][$x]['choices'][1]['answer'];
-            $the_answer_3[$x] = $the_final['kahoot']['questions'][$x]['choices'][2]['answer'];
-            $the_answer_4[$x] = $the_final['kahoot']['questions'][$x]['choices'][3]['answer'];
-            $the_correct_1[$x] = $the_final['kahoot']['questions'][$x]['choices'][0]['correct'];
-            $the_correct_2[$x] = $the_final['kahoot']['questions'][$x]['choices'][1]['correct'];
-            $the_correct_3[$x] = $the_final['kahoot']['questions'][$x]['choices'][2]['correct'];
-            $the_correct_4[$x] = $the_final['kahoot']['questions'][$x]['choices'][3]['correct'];
-            if($the_correct_1[$x]){
-                $the_answer_edit .= '1,';
-            }
-            if($the_correct_2[$x]){
-                $the_answer_edit .= '2,';
-            }
-            if($the_correct_3[$x]){
-                $the_answer_edit .= '3,';
-            }
-            if($the_correct_4[$x]){
-                $the_answer_edit .= '4,';
-            }
-            $the_correct_answer[] = rtrim($the_answer_edit, ',');
-            $the_times[] = ($the_final['kahoot']['questions'][$x]['time']) / 1000;
-        }
+    if(is_null($the_answer_4)){
+        $the_answer_4 = '';
     }
+    if(is_null($the_correct_3)){
+        $the_correct_3 = '';
+    }
+    if(is_null($the_correct_4)){
+        $the_correct_4 = '';
+    }
+    if($question['choices'][0]['correct']){
+        $the_answer_edit = '1,';
+    }
+    if($question['choices'][1]['correct']){
+        $the_answer_edit .= '2,';
+    }
+    if($question['choices'][2]['correct']){
+        $the_answer_edit .= '3,';
+    }
+    if($question['choices'][3]['correct']){
+        $the_answer_edit .= '4,';
+    }
+    $the_correct_answer[] = rtrim($the_answer_edit, ',');
+    $the_times[] = ($question['time']) / 1000;
 }
-$the_title = $the_final['card']['title'];
+$the_title = preg_replace("/[^a-zA-Z0-9 ]+/", "", $the_final['card']['title']);
 include_once("xlsxwriter.class.php");
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 header("Content-Disposition: attachment;filename=$the_title.xlsx");
@@ -73,13 +58,10 @@ $writer->writeSheetRow('Sheet 1', array('',"Remember: questions have a limit of 
 $writer->writeSheetRow('Sheet 1', array('',"And remember,  if you're not using Excel you need to export to .xlsx format before you upload to Kahoot!",'','','','','',''));
 $writer->writeSheetRow('Sheet 1', array('','','','','','','',''));
 $writer->writeSheetRow('Sheet 1', array('','Question - max 120 characters','Answer 1 - max 75 characters','Answer 2 - max 75 characters','Answer 3 - max 75 characters','Answer 4 - max 75 characters',"Time limit (sec) – 5, 10, 20, 30, 60, 90, 120, or 240 secs",'Correct answer(s) - choose at least one'));
-for($x=0;$x<5000;$x++){
-    if($the_questions[$x] == ''){
-        break;
-    }
-    else{
-        $writer->writeSheetRow('Sheet 1', array(($x + 1),$the_questions[$x],$the_answer_1[$x],$the_answer_2[$x],$the_answer_3[$x],$the_answer_4[$x],$the_times[$x],$the_correct_answer[$x],' '));
-    }
+$x=0;
+foreach($the_questions as $question){
+    $writer->writeSheetRow('Sheet 1', array(($x + 1),$the_questions[$x],$the_answer_1[$x],$the_answer_2[$x],$the_answer_3[$x],$the_answer_4[$x],$the_times[$x],$the_correct_answer[$x],' '));
+    $x++;
 }
-$writer->writeToStdOut();//like echo $writer->writeToString();
+$writer->writeToStdOut();
 ?>
